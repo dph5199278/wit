5df77d74d3114871284afba8ecdafc946ff5a12b
dph5199278 <dph5199278@163.com>
2021-10-24 21:04:28 +0800

    1. Refact properties to yml 2. Update dependencies 3. Refact druid config

--- contact-center/app/src/main/java/com/cs/wit/config/DruidConfiguration.java
+++ contact-center/app/src/main/java/com/cs/wit/config/DruidConfiguration.java
@@ -16,84 +16,68 @@
  */
 package com.cs.wit.config;
 
-import com.alibaba.druid.pool.DruidDataSource;
-import com.alibaba.druid.support.http.StatViewServlet;
-import com.alibaba.druid.support.http.WebStatFilter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import com.alibaba.druid.spring.boot.autoconfigure.properties.DruidStatProperties;
+import com.alibaba.druid.util.Utils;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DatabaseDriver;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.servlet.*;
+import java.io.IOException;
+
 @Configuration
-@ConditionalOnClass(com.alibaba.druid.pool.DruidDataSource.class)
-@ConditionalOnProperty(name = "spring.datasource.type", havingValue = "com.alibaba.druid.pool.DruidDataSource", matchIfMissing = true)
+@ConditionalOnWebApplication
+@AutoConfigureAfter(DruidDataSourceAutoConfigure.class)
+@ConditionalOnProperty(name = "spring.datasource.druid.stat-view-servlet.enabled", havingValue = "true", matchIfMissing = true)
 public class DruidConfiguration {
 
-    @SuppressWarnings("unchecked")
-    protected <T> T createDataSource(DataSourceProperties properties) {
-        return (T) properties.initializeDataSourceBuilder().type(DruidDataSource.class).build();
-    }
-
     /**
-     * @param properties 读入的配置
-     * @return DruidDataSource
-     * @see org.springframework.boot.autoconfigure.jdbc.DataSourceConfiguration.Tomcat 仿写的你可以去了解
+     * 方法名: removeDruidAdFilterRegistrationBean 方法描述: 除去页面底部的广告 @param
+     * properties @return
+     * org.springframework.boot.web.servlet.FilterRegistrationBean @throws
      */
-    @SuppressWarnings("JavadocReference")
+    @SuppressWarnings({ "rawtypes", "unchecked" })
     @Bean
-    @ConfigurationProperties("spring.datasource.druid")
-    public com.alibaba.druid.pool.DruidDataSource dataSource(DataSourceProperties properties) {
+    public FilterRegistrationBean removeDruidAdFilterRegistrationBean(DruidStatProperties properties) {
+        // 获取web监控页面的参数
+        DruidStatProperties.StatViewServlet config = properties.getStatViewServlet();
+        // 提取common.js的配置路径
+        String pattern = config.getUrlPattern() != null ? config.getUrlPattern() : "/druid/*";
+        String commonJsPattern = pattern.replaceAll("\\*", "js/common.js");
 
-        com.alibaba.druid.pool.DruidDataSource dataSource = createDataSource(properties);
+        final String filePath = "support/http/resources/js/common.js";
 
-        DatabaseDriver databaseDriver = DatabaseDriver.fromJdbcUrl(properties.determineUrl());
+        // 创建filter进行过滤
+        Filter filter = new Filter() {
+            @Override
+            public void init(FilterConfig filterConfig) throws ServletException {
+            }
 
-        String validationQuery = databaseDriver.getValidationQuery();
-        if (validationQuery != null) {
-            dataSource.setTestOnBorrow(true);
-            dataSource.setValidationQuery(validationQuery);
-        }
+            @Override
+            public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+                    throws IOException, ServletException {
+                chain.doFilter(request, response);
+                // 重置缓冲区，响应头不会被重置
+                response.resetBuffer();
+                // 获取common.js
+                String text = Utils.readFromResource(filePath);
+                // 正则替换banner, 除去底部的广告信息
+                text = text.replaceAll("<a.*?banner\"></a><br/>", "");
+                text = text.replaceAll("powered.*?shrek.wang</a>", "");
+                response.getWriter().write(text);
+            }
 
-        return dataSource;
-    }
-
-
-    /**
-     * 注册一个StatViewServlet
-     */
-    @Bean
-    public ServletRegistrationBean<StatViewServlet> DruidStatViewServle2() {
-        //org.springframework.boot.context.embedded.ServletRegistrationBean提供类的进行注册.
-        ServletRegistrationBean<StatViewServlet> servletRegistrationBean = new ServletRegistrationBean<>(new StatViewServlet(), "/druid/*");
-        //添加初始化参数：initParams
-        //白名单：
-//		servletRegistrationBean.addInitParameter("allow","127.0.0.1");
-        //IP黑名单 (存在共同时，deny优先于allow) : 如果满足deny的话提示:Sorry, you are not permitted to view this page.
-//		servletRegistrationBean.addInitParameter("deny","192.168.1.73");
-        //登录查看信息的账号密码.
-        servletRegistrationBean.addInitParameter("loginUsername", "admin");
-        servletRegistrationBean.addInitParameter("loginPassword", "123456");
-        //是否能够重置数据.
-        servletRegistrationBean.addInitParameter("resetEnable", "false");
-        return servletRegistrationBean;
-    }
-
-    /**
-     * 注册一个：filterRegistrationBean
-     */
-
-    @Bean
-    public FilterRegistrationBean<WebStatFilter> druidStatFilter2() {
-        FilterRegistrationBean<WebStatFilter> filterRegistrationBean = new FilterRegistrationBean<>(new WebStatFilter());
-        //添加过滤规则.
-        filterRegistrationBean.addUrlPatterns("/*");
-        //添加不需要忽略的格式信息.
-        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid2/*");
-        return filterRegistrationBean;
+            @Override
+            public void destroy() {
+            }
+        };
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(filter);
+        registrationBean.addUrlPatterns(commonJsPattern);
+        return registrationBean;
     }
 }
