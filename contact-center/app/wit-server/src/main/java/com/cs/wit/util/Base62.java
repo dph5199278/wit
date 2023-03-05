e152639d5daa476e8b9a5c5b29100de1fab49abc
Dely <dph5199278@163.com>
2023-03-05 09:24:19 +0800

    perf: Base62.java

--- contact-center/app/wit-server/src/main/java/com/cs/wit/util/Base62.java
+++ contact-center/app/wit-server/src/main/java/com/cs/wit/util/Base62.java
@@ -16,50 +16,58 @@
  */
 package com.cs.wit.util;
 
+import java.util.Objects;
 import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.commons.lang.StringUtils;
 
+/**
+ * The type Base 62.
+ */
 public class Base62 {
-	private static final int BINARY = 0x2;
 
 	private static final int NUMBER_61 = 0x0000003d;
-	
+
+	/**
+	 * The Digits.
+	 */
 	static final char[] DIGITS = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
 			'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',
 			'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
 			'X', 'Y', 'Z' };
 
 
+	/**
+	 * Encode string.
+	 *
+	 * @param value the value
+	 * @return the string
+	 */
 	public static String encode(long value){
-		return encode(String.valueOf(value)).toLowerCase() ;
+		return Objects.requireNonNull(encode(String.valueOf(value))).toLowerCase() ;
 	}
-     
-    public static String encode(String str){ 
-    	    String md5Hex = DigestUtils.md5Hex(str);
-    	    // 6 digit binary can indicate 62 letter & number from 0-9a-zA-Z
-    	    int binaryLength = 6 * 6;
-    	    long binaryLengthFixer = Long.valueOf(StringUtils.repeat("1", binaryLength), BINARY);
-    	    for (int i = 0; i < 4;) {
-    	      String subString = StringUtils.substring(md5Hex, i * 8, (i + 1) * 8);
-    	      subString = Long.toBinaryString(Long.valueOf(subString, 16) & binaryLengthFixer);
-    	      subString = StringUtils.leftPad(subString, binaryLength, "0");
-    	      StringBuilder sbBuilder = new StringBuilder();
-    	      for (int j = 0; j < 6; j++) {
-    	        String subString2 = StringUtils.substring(subString, j * 6, (j + 1) * 6);
-    	        int charIndex = Integer.valueOf(subString2, BINARY) & NUMBER_61;
-    	        sbBuilder.append(DIGITS[charIndex]);
-    	      }
-    	      String shortUrl = sbBuilder.toString();
-    	      if(shortUrl!=null){
-    	    	  return shortUrl;
-    	      }
-    	    }
-    	    // if all 4 possibilities are already exists
-    	    return null;
-    } 
-     
-    @SuppressWarnings("unused")
-	private static void print(Object messagr){ 
-        System.out.println(messagr); 
-    }  
+
+	/**
+	 * Encode string.
+	 *
+	 * @param str the str
+	 * @return the string
+	 */
+	public static String encode(String str){
+		// get md5 hex string
+		String md5Hex = DigestUtils.md5Hex(str);
+		// get md5 first 4 byte string
+		String md5HexFirst4Byte = md5Hex.substring(0, 8);
+		// parse to long
+		long md5HexFirst4ByteLong = Long.parseLong(md5HexFirst4Byte, 16);
+		StringBuilder encodedBuilder = new StringBuilder();
+		// 6 digit binary can indicate 62 letter & number from 0-9a-zA-Z
+		for (int j = 5; j >= 0; j--) {
+			int charIndex = (int)((md5HexFirst4ByteLong >> (j * 6)) & NUMBER_61);
+			encodedBuilder.append(DIGITS[charIndex]);
+		}
+		if(0 < encodedBuilder.length()) {
+			return encodedBuilder.toString();
+		}
+		// if all 4 possibilities are already exists
+		return null;
+	}
 }
