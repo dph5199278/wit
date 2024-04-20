74317c51e96dddbd43bf31622c0941050bd13fe8
Dely <dph5199278@163.com>
2024-04-20 14:40:08 +0800

    refactor: Simplify spring bean proxy
    Signed-off-by: Dely <dph5199278@163.com>

--- contact-center/app/wit-server/src/main/java/com/cs/wit/config/WebSecurityConfig.java
+++ contact-center/app/wit-server/src/main/java/com/cs/wit/config/WebSecurityConfig.java
@@ -1,7 +1,5 @@
 /*
- * Copyright (C) 2017 优客服-多渠道客服系统
- * Modifications copyright (C) 2018-2019 Chatopera Inc, <https://www.chatopera.com>
- * Modifications copyright (C) 2022-2024 Dely<https://github.com/dph5199278>
+ * Copyright (C) 2024 Dely<https://github.com/dph5199278>, All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +15,6 @@
  */
 package com.cs.wit.config;
 
-import jakarta.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -35,11 +32,39 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class WebSecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http,
-        Filter tokenInfoTokenFilterSecurityInterceptor,
-        Filter apiTokenFilterSecurityInterceptor) throws Exception {
-        return http.addFilterAfter(tokenInfoTokenFilterSecurityInterceptor, BasicAuthenticationFilter.class)
-            .addFilterAfter(apiTokenFilterSecurityInterceptor, BasicAuthenticationFilter.class)
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            // apiTokenFilterSecurityInterceptor
+            .addFilterAfter(
+                new ApiRequestMatchingFilter(new AntPathRequestMatcher("/api/**")),
+                BasicAuthenticationFilter.class)
+            // tokenInfoTokenFilterSecurityInterceptor
+            .addFilterAfter(
+                DelegateRequestMatchingFilter.builder()
+                // autoConfig
+                .addRequestMatcher(new AntPathRequestMatcher("/autoconfig/**"))
+                // configprops
+                .addRequestMatcher(new AntPathRequestMatcher("/configprops/**"))
+                // beans
+                .addRequestMatcher(new AntPathRequestMatcher("/beans/**"))
+                // dump
+                .addRequestMatcher(new AntPathRequestMatcher("/dump/**"))
+                // env
+                .addRequestMatcher(new AntPathRequestMatcher("/env/**"))
+                // health(Bypass actuator api)
+                //.addRequestMatcher(new AntPathRequestMatcher("/health/**"))
+                // info
+                //.addRequestMatcher(new AntPathRequestMatcher("/info/**"))
+                // mappings
+                .addRequestMatcher(new AntPathRequestMatcher("/mappings/**"))
+                // metrics(Bypass actuator api)
+                //.addRequestMatcher(new AntPathRequestMatcher("/metrics/**"))
+                // trace
+                .addRequestMatcher(new AntPathRequestMatcher("/trace/**"))
+                // druid
+                .addRequestMatcher(new AntPathRequestMatcher("/druid/**"))
+                .build(),
+                ApiRequestMatchingFilter.class)
             // https://github.com/spring-projects/spring-security/issues/13568
             .authorizeHttpRequests(authorize -> authorize.requestMatchers("/**").permitAll())
             .csrf(csrf -> csrf.csrfTokenRequestHandler(new CryptoCsrfTokenRequestAttributeHandler()))
@@ -48,37 +73,4 @@ public class WebSecurityConfig {
             ;
     }
 
-    @Bean("tokenInfoTokenFilterSecurityInterceptor")
-    public Filter tokenInfoTokenFilterSecurityInterceptor() {
-        return DelegateRequestMatchingFilter.builder()
-            // autoConfig
-            .addRequestMatcher(new AntPathRequestMatcher("/autoconfig/**"))
-            // configprops
-            .addRequestMatcher(new AntPathRequestMatcher("/configprops/**"))
-            // beans
-            .addRequestMatcher(new AntPathRequestMatcher("/beans/**"))
-            // dump
-            .addRequestMatcher(new AntPathRequestMatcher("/dump/**"))
-            // env
-            .addRequestMatcher(new AntPathRequestMatcher("/env/**"))
-            // health(Bypass actuator api)
-            //.addRequestMatcher(new AntPathRequestMatcher("/health/**"))
-            // info
-            //.addRequestMatcher(new AntPathRequestMatcher("/info/**"))
-            // mappings
-            .addRequestMatcher(new AntPathRequestMatcher("/mappings/**"))
-            // metrics(Bypass actuator api)
-            //.addRequestMatcher(new AntPathRequestMatcher("/metrics/**"))
-            // trace
-            .addRequestMatcher(new AntPathRequestMatcher("/trace/**"))
-            // druid
-            .addRequestMatcher(new AntPathRequestMatcher("/druid/**"))
-            .build()
-            ;
-    }
-
-    @Bean("apiTokenFilterSecurityInterceptor")
-    public Filter apiTokenFilterSecurityInterceptor() {
-        return new ApiRequestMatchingFilter(new AntPathRequestMatcher("/api/**"));
-    }
 }
