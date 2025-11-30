ec7e6c7d26dea4258e54ba0d30be1b674e374e3d
Dely <dph5199278@163.com>
2025-11-30 16:45:48 +0800

    test(wit-ip2region): support IPV6
    Signed-off-by: Dely <dph5199278@163.com>

--- contact-center/app/wit-ip2region/src/test/java/com/cs/wit/util/ip/IPTest.java
+++ contact-center/app/wit-ip2region/src/test/java/com/cs/wit/util/ip/IPTest.java
@@ -7,13 +7,38 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * Unit test for simple App.
  */
 public class IPTest {
+
     /**
-     * Rigorous Test :-)
+     * IPV4 test
      */
     @Test
-    public void ip() {
+    public void ipv4() {
+        IP ip = ip("157.0.45.35");
+        System.out.println("IPV4: ");
+        System.out.println(ip.getCountry());
+        System.out.println(ip.getProvince());
+        System.out.println(ip.getCity());
+        System.out.println(ip.getIsp());
+        System.out.println(ip.getRegion());
+    }
+
+    /**
+     * IPV6 test
+     */
+    @Test
+    public void ipv6() {
+        IP ip = ip("2408:843c:4600::1");
+        System.out.println("IPV6: ");
+        System.out.println(ip.getCountry());
+        System.out.println(ip.getProvince());
+        System.out.println(ip.getCity());
+        System.out.println(ip.getIsp());
+        System.out.println(ip.getRegion());
+    }
+
+    private IP ip(String ip) {
         IPTools tools = new IPTools(new AnnotationConfigApplicationContext());
         tools.setup();
-        System.out.println(tools.findGeography("157.0.45.35"));
+        return tools.findGeography(ip);
     }
 }
