f79f8d34479daa11192058bf6b3f74dff78a273d
Dely <dph5199278@163.com>
2025-12-20 18:18:23 +0800

    refactor: remove deprecated proxy annotation
    Signed-off-by: Dely <dph5199278@163.com>

--- contact-center/app/wit-server/src/main/java/com/cs/wit/model/UserHistory.java
+++ contact-center/app/wit-server/src/main/java/com/cs/wit/model/UserHistory.java
@@ -24,11 +24,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Proxy;
 
 @Entity
 @Table(name = "uk_userevent")
-@Proxy(lazy = false)
 public class UserHistory implements java.io.Serializable {
     /**
      *
